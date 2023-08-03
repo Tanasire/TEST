ede6028ad44bb36e1f0f9e8c96a324a7628955c5
Hyewon <tfow1103@gmail.com>
2023-08-03 17:40:54 +0900

    2023-08-03 혜원업로드

--- Test/src/test/Newtest.java
+++ Test/src/test/Newtest.java
@@ -2,5 +2,13 @@ package test;
 public class Newtest {
 public static void main(String[] args) {
 	System.out.println("어성권 새로운 클래스 만들어봤습니다.");
+	
+	System.out.println("안녕하세요");
+	System.out.println("Hello");
+	System.out.println("Hola");
+	System.out.println("Boosasddsfdur");
+	// 왜 안 될까
+	// ??????
+	
 }
 }
